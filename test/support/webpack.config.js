4ee92612fa836699be416916df91eaddf60c5344
Brady Holt <brady.holt@gmail.com>
2018-04-05 08:50:42 -0500

    Upgrade deps, include webpack

--- test/support/webpack.config.js
+++ test/support/webpack.config.js
@@ -1,21 +1,16 @@
 const webpack = require("webpack");
 const path = require("path");
 const fs = require("fs");
-const nodeExternals = require("webpack-node-externals");
 
 var config = {
   /*
     mocha-webpack will set entry/output options at runtime so we don't need to set them here
   entry:
   output: */
-  target: "node",
   devtool: "cheap-module-eval-source-map",
   /*
     Use webpack-node-externals to prevent bundling anything referenced in node_modules.
     At runtime, these modules can be loaded by Node and therefore do not need to be bundled by Webpack. */
-  externals: [
-    nodeExternals({ modulesDir: path.join(__dirname, "../../node_modules") })
-  ],
   resolve: {
     extensions: [".ts", ".js", ".vue"]
   },
